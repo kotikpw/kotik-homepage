19046bf7b64a56be72e84798e4d7c711ccf28605
Barnaba Turek <barnabaturek@gmail.com>
2013-03-22 21:02:44 +0100

    rails project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KotikHomepage::Application.config.secret_token = 'c6431e3f57e4c1dbd451ab2c9ad5f3a547581de9cb7e0c12e47437a7d268e79883bf4127ae474762c6efe400c87a50f12bdf7adddbef1da007431abc0c926725'
